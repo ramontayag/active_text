03ca7a3b0d940a1440d25cf57a82082fd0d71b13
Ramon Tayag <ramon@tayag.net>
2011-04-15 17:27:34 +0800

    changed api, see readme

--- lib/active_text/base.rb
+++ lib/active_text/base.rb
@@ -12,12 +12,19 @@ module ActiveText
       options[:comment] ||= /\/\//
       options[:context] ||= "(^(?:#{options[:comment]}\s@.*#{options[:eol]}){#{options[:context_lines]}})"
       @options = options
+
+      # instantiate all variables
+      @text.scan(/^\${1}(.+): .+;/).flatten.each do |variable_name|
+        if has_context?(variable_name)
+          variable = ActiveText::Variable.new(variable_name, context_of(variable_name), @options[:comment])
+          @variables.merge!({variable_name.to_sym => variable})
+        end
+      end
     end
 
     def update_attributes(args)
       args.each do |k, v|
-        send(k) # Instantiate it, so it will exist in @variables
-        @variables[k].value = v unless @variables[k].nil? || v.nil?
+        send "#{k.to_s}=", v
       end
     end
 
@@ -29,21 +36,34 @@ module ActiveText
       @text
     end
 
+    def [](key)
+      variable = @variables[key]
+      {:name => variable.name, :description => variable.description, :value => variable.value, :kind => variable.kind}
+    end
+
+    def attributes
+      h = {}
+      @variables.each do |key, variable|
+        h.merge!({key => variable.value})
+      end
+      h
+    end
+
     protected
 
     # Whenever a variable is requested for, it falls into this.
-    def method_missing(method_name)
-      if method_name.to_s =~ /[\w]+/
-        context = context_of(method_name)
-
-        # If there's no context (no variable with the proper
-        # commented metadata), then it should not be accessible
-        # and it won't be accessible
-        if context.nil? || context == ""
-          nil
+    def method_missing(method_name, *args, &block)
+      attr_key = method_name.to_s.sub(/\=$/, '').to_sym
+      variable = @variables[attr_key]
+      if variable
+        if method_name.to_s =~ /\=$/
+          value = args[0]
+          variable.value = value unless value.nil?
         else
-          @variables[method_name] ||= ActiveText::Variable.new(method_name, context, @options[:comment])
+          variable.value
         end
+      else
+        raise NoMethodError, "Variable does not exist"
       end
     end
 
@@ -55,6 +75,11 @@ module ActiveText
       @text =~ /^#{@options[:context]}(#{regexp})/
       before, match = $1, $2
       "#{before}#{match}"
+    end
+
+    def has_context?(s)
+      context = context_of(s)
+      !(context.nil? || context == "")
     end
   end
 end
--- spec/active_text_spec.rb
+++ spec/active_text_spec.rb
@@ -17,15 +17,15 @@ $mbc: #555;}
     it "should be able to set variables and return them properly" do
       @atb = ActiveText::Base.new(@text)
 
-      @atb.mbc.name.should == "Masthead BG Color"
-      @atb.mbc.kind.should == "color"
-      @atb.mbc.description.should == "Background color."
-      @atb.mbc.value.should == "#555"
+      @atb[:mbc][:name].should == "Masthead BG Color"
+      @atb[:mbc][:kind].should == "color"
+      @atb[:mbc][:description].should == "Background color."
+      @atb[:mbc][:value].should == "#555"
 
-      @atb.mbc2.name.should == "Masthead Background Image"
-      @atb.mbc2.kind.should == "file"
-      @atb.mbc2.description.should == "Background image."
-      @atb.mbc2.value.should == %Q("http://someurl.com/image.jpg")
+      @atb[:mbc2][:name].should == "Masthead Background Image"
+      @atb[:mbc2][:kind].should == "file"
+      @atb[:mbc2][:description].should == "Background image."
+      @atb[:mbc2][:value].should == %Q("http://someurl.com/image.jpg")
     end
   end
 
@@ -36,15 +36,15 @@ $mbc: #555;}
 // @description Background image.
 $mbc2: "http://someurl.com/image.jpg";}
       @s = ActiveText::Base.new(text)
-      @s.mbc2.value = %Q("Another URL")
-      @s.mbc2.value.should == %Q("Another URL")
+      @s.mbc2 = %Q("Another URL")
+      @s.mbc2.should == %Q("Another URL")
 
       rendered_text = @s.render
       rendered_text.should_not match(/http:\/\/someurl\.com\/image\.jpg/)
       rendered_text.should match(/\$mbc2: "Another URL";/)
 
-      @s.mbc2.value = %Q("Some third URL")
-      @s.mbc2.value.should == %Q("Some third URL")
+      @s.mbc2 = %Q("Some third URL")
+      @s.mbc2.should == %Q("Some third URL")
       rendered_text = @s.render
       rendered_text.should_not match(/\$mbc2: "Another URL";/)
       rendered_text.should match(/\$mbc2: "Some third URL";/)
@@ -60,20 +60,43 @@ $mbc2: "http://someurl.com/image.jpg";}
 // @description Background image.
 $mbc2: "Another URL";}
       @s = ActiveText::Base.new(text_old)
-      @s.mbc2.value = %Q("Another URL")
+      @s.mbc2 = %Q("Another URL")
       @s.render.should == text_new
     end
   end
 
   describe "when there's no metadata" do
-    it "should not allow reading of data" do
-      text = %{// @name Masthead Background Image
+    before do
+      @text = %{// @name Masthead Background Image
 // @kind file
 // @description Background image.
 $mbc2: "http://someurl.com/image.jpg";
 $mbc: #555;}
-      @s = ActiveText::Base.new(text)
-      @s.mbc.should be_nil
+      @s = ActiveText::Base.new(@text)
+    end
+
+    it "should not allow reading of data" do
+      lambda {@s.mbc}.should raise_error(NoMethodError)
+    end
+  end
+
+  describe ".attributes" do
+    it ".attributes should return an empty hash when there are no valid variables" do
+      @text = %{
+$mbc2: "http://someurl.com/image.jpg";
+$mbc: #555;}
+      @s = ActiveText::Base.new(@text)
+      @s.attributes.should == {}
+    end
+
+    it "should only return the valid variables" do
+      @text = %{// @name Masthead Background Image
+// @kind file
+// @description Background image.
+$mbc2: "http://someurl.com/image.jpg";
+$mbc: #555;}
+      @s = ActiveText::Base.new(@text)
+      @s.attributes.should == {:mbc2 => %Q("http://someurl.com/image.jpg")}
     end
   end
 
@@ -89,8 +112,29 @@ $mbc2: "http://someurl.com/image.jpg";
 $mbc: #555;}
     @s = ActiveText::Base.new(text)
     @s.update_attributes(:mbc2 => %Q("Another URL"), :mbc => nil)
-    @s.mbc2.value.should == %Q("Another URL")
-    @s.mbc.value.should == "#555"
+    @s.mbc2.should == %Q("Another URL")
+    @s.mbc.should == "#555"
   end
 
+  it "should have setter method for variables" do
+    text = %{// @name Masthead Background Image
+// @kind file
+// @description Background image.
+$mbc2: "http://someurl.com/image.jpg";
+}
+    @s = ActiveText::Base.new(text)
+    @s.mbc2 = "44"
+    @s.mbc2.should == "44"
+  end
+
+  it "should not allow to set to nil" do
+    text = %{// @name Masthead Background Image
+// @kind file
+// @description Background image.
+$mbc2: "http://someurl.com/image.jpg";
+}
+    @s = ActiveText::Base.new(text)
+    @s.mbc2 = nil
+    @s.mbc2.should == %Q("http://someurl.com/image.jpg")
+  end
 end
